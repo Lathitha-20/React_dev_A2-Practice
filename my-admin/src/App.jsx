b28d6ca02acfb684ebdd6599955d9f446dd8e009
Lathitha-20 <lathiesiyekwana@gmail.com>
2025-11-08 18:50:37 +0000

    adding my trialbase provider

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -3,7 +3,7 @@ import { Route } from "react-router-dom";
 import { createTrailbaseProvider } from "./ra-trailbase";
 import { CreateUser, LoginPage } from "./create_user";
 
-const TRAILBASE_URL = "http://fantastic-garbanzo-jj4p75qppqww2p65q-4000.app.github.dev/";
+const TRAILBASE_URL = "https://fantastic-rotary-phone-wr9q57gqqvjr3grq9-4000.app.github.dev/";
 const { dataProvider, authProvider } = await createTrailbaseProvider(
   TRAILBASE_URL,
 );
