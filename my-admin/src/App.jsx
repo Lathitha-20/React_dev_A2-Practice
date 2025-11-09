afa306855e78e290ed96ffbf4ecfced9bbd9a824
Lathitha-20 <lathiesiyekwana@gmail.com>
2025-11-09 09:55:13 +0000

    editing my employee show

--- my-admin/src/App.jsx
+++ my-admin/src/App.jsx
@@ -1,7 +1,10 @@
-import { ListGuesser, Admin, CustomRoutes } from "react-admin";
+import { Admin,EditGuesser,Layout, ListGuesser, CustomRoutes, Resource, ShowGuesser } from "react-admin";
 import { Route } from "react-router-dom";
 import { createTrailbaseProvider } from "./ra-trailbase";
 import { CreateUser, LoginPage } from "./create_user";
+import { EmployeeCreate } from "./resources/employees/EmployeeCreate";
+import { EmployeeEdit } from "./resources/employees/EmployeeEdit";
+import { EmployeeShow } from "module";
 
 const TRAILBASE_URL = "https://fantastic-rotary-phone-wr9q57gqqvjr3grq9-4000.app.github.dev/";
 const { dataProvider, authProvider } = createTrailbaseProvider(TRAILBASE_URL);
@@ -12,7 +15,7 @@ function App() {
       loginPage={LoginPage} dataProvider={dataProvider} authProvider={authProvider}
     >
       <Resource name="employees" list={ListGuesser} edit={EmployeeEdit} create={EmployeeCreate}/>
-      
+
       <CustomRoutes noLayout>
         <Route path="/create-user" element={<CreateUser />} />
       </CustomRoutes>
